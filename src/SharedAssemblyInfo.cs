59e0658d06ce36a7339a0ae0d20ceb04bec65668
Giscard Biamby <giscardo@yahoo.com>
2013-10-18 20:56:16 -0700

    Updated Cassette.Nancy version # to 2.1.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyInformationalVersion("2.0.0")]
-[assembly: AssemblyVersion("2.0.0.*")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.1.0")]
+[assembly: AssemblyVersion("2.1.0.*")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
