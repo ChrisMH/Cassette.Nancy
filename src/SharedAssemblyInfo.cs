b2fe32414f65cf5fd665a498568ddce86428360c
Chris Hogan <hogancm@gmail.com>
2014-07-31 11:07:25 -0400

    Updated version

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyInformationalVersion("2.1.1")]
-[assembly: AssemblyVersion("2.1.1.*")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: AssemblyInformationalVersion("2.2.1")]
+[assembly: AssemblyVersion("2.2.1.*")]
+[assembly: AssemblyFileVersion("2.2.1.0")]
